9cd481faa1767954ea1803ab47aa3c0f2af76b6a
KaelTorre <kaeltorre@gmail.com>
2024-10-24 20:30:17 -0500

    tercer commit

---
+++ src/models/User.js
@@ -0,0 +1,9 @@
+class User{
+    static tableName = 'usuario';
+    constructor(id,name,email){
+        this.id = id;
+        this.name = name;
+        this.correo = correo;
+    }
+}
+module.exports = User;
