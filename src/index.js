1240a9b6e28da4019fd47987b368790a1a1a0bb8
KaelTorre <kaeltorre@gmail.com>
2024-11-14 20:56:07 -0500

    decimo commit
    nuevo back que conecta y permite Angular

--- src/index.js
+++ src/index.js
@@ -2,9 +2,24 @@ require('dotenv').config();
 const express = require('express');
 const userController = require('./controllers/userControllers');
 const rolController = require('./controllers/rolControllers');
+const helmet  = require('helmet');
 
 const app = express();
 app.use(express.json());
+app.use(helmet());
+
+app.use(helmet.referrerPolicy({
+    policy: 'strict-origin-when-cross-origin'
+}));
+
+  // Configurar CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:4200'); // Ajusta esto a la URL de tu frontend
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
+
 app.use('/api/users', userController);
 app.use('/api/rols', rolController);
 
